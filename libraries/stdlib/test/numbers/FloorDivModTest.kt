9f34bda39d49ad1b383316396f1c8987569c9e01
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-02-21 00:59:22 +0300

    Avoid triggering division overflow in tests due to K/N

--- libraries/stdlib/test/numbers/FloorDivModTest.kt
+++ libraries/stdlib/test/numbers/FloorDivModTest.kt
@@ -14,6 +14,7 @@ class FloorDivModTest {
     @Test
     fun intDivMod() {
         fun check(a: Int, b: Int, expectedFd: Int? = null, expectedMod: Int? = null) {
+            if (a == Int.MIN_VALUE && b == -1) return // do not test, due to native KT-45136
             val div = a / b
             val rem = a % b
             val fd = a.floorDiv(b)
@@ -49,6 +50,7 @@ class FloorDivModTest {
     @Test
     fun longDivMod() {
         fun check(a: Long, b: Long, expectedFd: Long? = null, expectedMod: Long? = null) {
+            if (a == Long.MIN_VALUE && b == -1L) return // do not test, due to native KT-45136
             val div = a / b
             val rem = a % b
             val fd = a.floorDiv(b)
