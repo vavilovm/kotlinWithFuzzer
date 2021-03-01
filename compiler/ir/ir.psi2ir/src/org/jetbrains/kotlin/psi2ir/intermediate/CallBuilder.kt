MTJJ62/kotlin

[PSI2IR] Generate IR for functions and calls with context receivers

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/CallBuilder.kt
@@ -83,11 +83,11 @@ val CallBuilder.explicitReceiverType: KotlinType?
 fun CallBuilder.setExplicitReceiverValue(explicitReceiverValue: IntermediateValue) {
     val previousCallReceiver = callReceiver
     callReceiver = object : CallReceiver {
-        override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression {
-            return previousCallReceiver.call { dispatchReceiverValue, _ ->
+        override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression {
+            return previousCallReceiver.call { dispatchReceiverValue, _, contextReceiverValues ->
                 val newDispatchReceiverValue = if (hasExtensionReceiver) dispatchReceiverValue else explicitReceiverValue
                 val newExtensionReceiverValue = if (hasExtensionReceiver) explicitReceiverValue else null
-                withDispatchAndExtensionReceivers(newDispatchReceiverValue, newExtensionReceiverValue)
+                withDispatchAndExtensionReceivers(newDispatchReceiverValue, newExtensionReceiverValue, contextReceiverValues)
             }
         }
     }

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/IrUtils.kt
@@ -27,8 +27,8 @@ fun IrVariable.loadAt(startOffset: Int, endOffset: Int): IrExpression =
 
 fun CallReceiver.adjustForCallee(callee: CallableMemberDescriptor): CallReceiver =
     object : CallReceiver {
-        override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression =
-            this@adjustForCallee.call { dispatchReceiverValue, extensionReceiverValue ->
+        override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression =
+            this@adjustForCallee.call { dispatchReceiverValue, extensionReceiverValue, contextReceiverValues ->
                 val numReceiversPresent = listOfNotNull(dispatchReceiverValue, extensionReceiverValue).size
                 val numReceiversExpected = listOfNotNull(callee.dispatchReceiverParameter, callee.extensionReceiverParameter).size
                 if (numReceiversPresent != numReceiversExpected)
@@ -46,7 +46,8 @@ fun CallReceiver.adjustForCallee(callee: CallableMemberDescriptor): CallReceiver
                         dispatchReceiverValue != null && callee.dispatchReceiverParameter == null -> dispatchReceiverValue
                         else -> extensionReceiverValue
                     }
-                withDispatchAndExtensionReceivers(newDispatchReceiverValue, newExtensionReceiverValue)
+                // TODO: Create new context receiver values
+                withDispatchAndExtensionReceivers(newDispatchReceiverValue, newExtensionReceiverValue, contextReceiverValues)
             }
     }
 

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/SafeCallReceiver.kt
@@ -31,11 +31,13 @@ class SafeCallReceiver(
     val startOffset: Int,
     val endOffset: Int,
     val extensionReceiver: IntermediateValue?,
+    // TODO: Use context receivers
+    val contextReceivers: List<IntermediateValue>,
     val dispatchReceiver: IntermediateValue?,
     val isStatement: Boolean
 ) : CallReceiver {
 
-    override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression {
+    override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression {
         val irTmp = generator.scope.createTemporaryVariable(extensionReceiver?.load() ?: dispatchReceiver!!.load(), "safe_receiver")
         val safeReceiverValue = VariableLValue(generator.context, irTmp)
 
@@ -49,7 +51,7 @@ class SafeCallReceiver(
             extensionReceiverValue = null
         }
 
-        val irResult = withDispatchAndExtensionReceivers(dispatchReceiverValue, extensionReceiverValue)
+        val irResult = withDispatchAndExtensionReceivers(dispatchReceiverValue, extensionReceiverValue, emptyList())
 
         val resultType = if (isStatement) generator.context.irBuiltIns.unitType else irResult.type.makeNullable()
 
@@ -77,8 +79,9 @@ fun IrExpression.safeCallOnDispatchReceiver(
     SafeCallReceiver(
         generator, startOffset, endOffset,
         extensionReceiver = null,
+        contextReceivers = emptyList(),
         dispatchReceiver = OnceExpressionValue(this),
         isStatement = false
-    ).call { dispatchReceiverValue, _ ->
+    ).call { dispatchReceiverValue, _, _ ->
         ifNotNull(dispatchReceiverValue!!.load())
     }

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/SimpleCallReceiver.kt
@@ -20,10 +20,11 @@ import org.jetbrains.kotlin.ir.expressions.IrExpression
 
 class SimpleCallReceiver(
     private val dispatchReceiverValue: IntermediateValue?,
-    private val extensionReceiverValue: IntermediateValue?
+    private val extensionReceiverValue: IntermediateValue?,
+    private val contextReceiverValues: List<IntermediateValue>
 ) : CallReceiver {
 
-    override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression {
-        return withDispatchAndExtensionReceivers(dispatchReceiverValue, extensionReceiverValue)
+    override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression {
+        return withDispatchAndExtensionReceivers(dispatchReceiverValue, extensionReceiverValue, contextReceiverValues)
     }
 }

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/Values.kt
@@ -36,5 +36,5 @@ interface AssignmentReceiver {
 }
 
 interface CallReceiver {
-    fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression
+    fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression
 }

vavilovm/kotlinWithFuzzer

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/CallBuilder.kt
@@ -83,11 +83,11 @@ val CallBuilder.explicitReceiverType: KotlinType?
 fun CallBuilder.setExplicitReceiverValue(explicitReceiverValue: IntermediateValue) {
     val previousCallReceiver = callReceiver
     callReceiver = object : CallReceiver {
-        override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression {
-            return previousCallReceiver.call { dispatchReceiverValue, _ ->
+        override fun call(withDispatchAndExtensionAndContextReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression {
+            return previousCallReceiver.call { dispatchReceiverValue, _, contextReceiverValues ->
                 val newDispatchReceiverValue = if (hasExtensionReceiver) dispatchReceiverValue else explicitReceiverValue
                 val newExtensionReceiverValue = if (hasExtensionReceiver) explicitReceiverValue else null
-                withDispatchAndExtensionReceivers(newDispatchReceiverValue, newExtensionReceiverValue)
+                withDispatchAndExtensionAndContextReceivers(newDispatchReceiverValue, newExtensionReceiverValue, contextReceiverValues)
             }
         }
     }

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/IrUtils.kt
@@ -27,8 +27,8 @@ fun IrVariable.loadAt(startOffset: Int, endOffset: Int): IrExpression =
 
 fun CallReceiver.adjustForCallee(callee: CallableMemberDescriptor): CallReceiver =
     object : CallReceiver {
-        override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression =
-            this@adjustForCallee.call { dispatchReceiverValue, extensionReceiverValue ->
+        override fun call(withDispatchAndExtensionAndContextReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression =
+            this@adjustForCallee.call { dispatchReceiverValue, extensionReceiverValue, contextReceiverValues ->
                 val numReceiversPresent = listOfNotNull(dispatchReceiverValue, extensionReceiverValue).size
                 val numReceiversExpected = listOfNotNull(callee.dispatchReceiverParameter, callee.extensionReceiverParameter).size
                 if (numReceiversPresent != numReceiversExpected)
@@ -46,7 +46,8 @@ fun CallReceiver.adjustForCallee(callee: CallableMemberDescriptor): CallReceiver
                         dispatchReceiverValue != null && callee.dispatchReceiverParameter == null -> dispatchReceiverValue
                         else -> extensionReceiverValue
                     }
-                withDispatchAndExtensionReceivers(newDispatchReceiverValue, newExtensionReceiverValue)
+                // TODO: Create new context receiver values
+                withDispatchAndExtensionAndContextReceivers(newDispatchReceiverValue, newExtensionReceiverValue, contextReceiverValues)
             }
     }
 

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/SafeCallReceiver.kt
@@ -31,11 +31,13 @@ class SafeCallReceiver(
     val startOffset: Int,
     val endOffset: Int,
     val extensionReceiver: IntermediateValue?,
+    // TODO: Use context receivers
+    val contextReceivers: List<IntermediateValue>,
     val dispatchReceiver: IntermediateValue?,
     val isStatement: Boolean
 ) : CallReceiver {
 
-    override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression {
+    override fun call(withDispatchAndExtensionAndContextReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression {
         val irTmp = generator.scope.createTemporaryVariable(extensionReceiver?.load() ?: dispatchReceiver!!.load(), "safe_receiver")
         val safeReceiverValue = VariableLValue(generator.context, irTmp)
 
@@ -49,7 +51,7 @@ class SafeCallReceiver(
             extensionReceiverValue = null
         }
 
-        val irResult = withDispatchAndExtensionReceivers(dispatchReceiverValue, extensionReceiverValue)
+        val irResult = withDispatchAndExtensionAndContextReceivers(dispatchReceiverValue, extensionReceiverValue, emptyList())
 
         val resultType = if (isStatement) generator.context.irBuiltIns.unitType else irResult.type.makeNullable()
 
@@ -77,8 +79,9 @@ fun IrExpression.safeCallOnDispatchReceiver(
     SafeCallReceiver(
         generator, startOffset, endOffset,
         extensionReceiver = null,
+        contextReceivers = emptyList(),
         dispatchReceiver = OnceExpressionValue(this),
         isStatement = false
-    ).call { dispatchReceiverValue, _ ->
+    ).call { dispatchReceiverValue, _, _ ->
         ifNotNull(dispatchReceiverValue!!.load())
     }

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/SimpleCallReceiver.kt
@@ -20,10 +20,11 @@ import org.jetbrains.kotlin.ir.expressions.IrExpression
 
 class SimpleCallReceiver(
     private val dispatchReceiverValue: IntermediateValue?,
-    private val extensionReceiverValue: IntermediateValue?
+    private val extensionReceiverValue: IntermediateValue?,
+    private val contextReceiverValues: List<IntermediateValue>
 ) : CallReceiver {
 
-    override fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression {
-        return withDispatchAndExtensionReceivers(dispatchReceiverValue, extensionReceiverValue)
+    override fun call(withDispatchAndExtensionAndContextReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression {
+        return withDispatchAndExtensionAndContextReceivers(dispatchReceiverValue, extensionReceiverValue, contextReceiverValues)
     }
 }

compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/intermediate/Values.kt
@@ -36,5 +36,5 @@ interface AssignmentReceiver {
 }
 
 interface CallReceiver {
-    fun call(withDispatchAndExtensionReceivers: (IntermediateValue?, IntermediateValue?) -> IrExpression): IrExpression
+    fun call(withDispatchAndExtensionAndContextReceivers: (IntermediateValue?, IntermediateValue?, List<IntermediateValue>) -> IrExpression): IrExpression
 }